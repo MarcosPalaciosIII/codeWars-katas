a6d37d8264253083f492c069f0478e7fcd70e1cf
Marcos <mp2772@gmail.com>
2020-06-20 12:35:04 -0400

    added code for even or odd solution

--- Even-or-Odd/script.js
+++ Even-or-Odd/script.js
@@ -2,9 +2,7 @@
 const even_or_odd = (number) => (number % 2 === 0 ? "Even" : "Odd");
 
 // best practice solution
-
 // const even_or_odd = n => (n % 2) ? 'Odd' : 'Even';
 
 // clever solution
-
 // var even_or_odd = n => n & 1 ? 'Odd' : 'Even';
